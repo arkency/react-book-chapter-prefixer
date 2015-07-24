4749d8398f7941491c6146cc5138de3123a0be3b
Fernando <krajka.bartosz@gmail.com>
2015-07-24 13:52:33 +0200

    Making Prefixer component to render exactly the HTML template's body. Since we use JSX, we can almost copy&paste that HTML, remembering only to replace every 'class' attribute by 'className'. Seriously, it's that simple.

--- app/components/Prefixer.js
+++ app/components/Prefixer.js
@@ -2,8 +2,28 @@ import React from 'react';
 
 class Prefixer extends React.Component {
   render () {
-    return null;
+    return (
+      <div className="form-horizontal">
+        <div className="form-group">
+          <label for="selectbox" className="col-sm-3 control-label">Country</label>
+          <div className="col-sm-3">
+            <select className="form-control" id="selectbox">
+              <option value="Poland">Poland</option>
+            </select>
+          </div>
+        </div>
+        <div className="form-group">
+          <label for="input-text" className="col-sm-3 control-label">Phone number</label>
+          <div className="col-sm-3">
+            <div className="input-group">
+              <span className="input-group-addon">+48</span>
+              <input type="text" className="form-control" id="input-text" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
-
+    
 export default Prefixer;
