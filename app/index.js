feea641b8a44bf71c919737c2e3534e496089ad5
Paweł Pacana <pawel.pacana@gmail.com>
2017-07-21 22:42:22 +0200

    Upgrade React dependency.

--- app/index.js
+++ app/index.js
@@ -3,16 +3,17 @@ import './styles.css';
 
 import debug from 'debug';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Prefixer from './components/Prefixer';
 
 const ELEMENT1 = document.createElement('div');
 document.body.appendChild(ELEMENT1);
-React.render(<Prefixer />, ELEMENT1);
+ReactDOM.render(<Prefixer />, ELEMENT1);
 
 const ELEMENT2 = document.createElement('div');
 document.body.appendChild(ELEMENT2);
-React.render(<Prefixer initialCountry={"Serbia"} />, ELEMENT2);
+ReactDOM.render(<Prefixer initialCountry={"Serbia"} />, ELEMENT2);
 
 const ELEMENT3 = document.createElement('div');
 document.body.appendChild(ELEMENT3);
-React.render(<Prefixer initialCountry={"Narnia"} />, ELEMENT3);
+ReactDOM.render(<Prefixer initialCountry={"Narnia"} />, ELEMENT3);
