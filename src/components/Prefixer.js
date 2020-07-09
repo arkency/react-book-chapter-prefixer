6b6804c10d4cb610a54188e0b3fc66df9a3eee9d
Tomasz Patrzek <patrzek.tomasz@gmail.com>
2020-07-09 15:57:36 +0200

    Migrate to functional components

--- src/components/Prefixer.js
+++ src/components/Prefixer.js
@@ -1,56 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-let prefixes = {
+const prefixes = {
     'Germany': '+49',
     'Poland': '+48',
     'Serbia': '+381'
 };
 
-class Prefixer extends React.Component {
-  constructor(props) {
-    super(props);
+function Prefixer(props) {
+  const [country, changeCountry] = useState(props.initialCountry || "Poland")
 
-    let country = this.props.initialCountry || "Poland";
-    this.state = { country: country };
-
-    this.handleChange = this.handleChange.bind(this);
+  const handleChange = e => {
+    const newCountry = e.target.value;
+    changeCountry(newCountry);
   }
 
-  handleChange(e) {
-    let newCountry = e.target.value;
-    this.setState({ country: newCountry });
-  }
+  const countryItem = (country, index) =>
+    <option value={country} key={index}>{country}</option>
 
-  countryItem(country, index) {
-    return (
-      <option value={country} key={index}>{country}</option>
-    );
-  }
-
-  render () {
-    let prefix = prefixes[this.state.country];
-    return (
-      <div className="form-horizontal">
-        <div className="form-group">
-          <label htmlFor="selectbox" className="col-sm-3 control-label">Country</label>
-          <div className="col-sm-3">
-            <select className="form-control" id="selectbox" onChange={this.handleChange} value={this.state.country}>
-              {Object.keys(prefixes).map(this.countryItem)}
-            </select>
-          </div>
+  const prefix = prefixes[country];
+  return (
+    <div className="form-horizontal">
+      <div className="form-group">
+        <label htmlFor="selectbox" className="col-sm-3 control-label">Country</label>
+        <div className="col-sm-3">
+          <select className="form-control" id="selectbox" onChange={handleChange} value={country}>
+            {Object.keys(prefixes).map(countryItem)}
+          </select>
         </div>
-        <div className="form-group">
-          <label htmlFor="input-text" className="col-sm-3 control-label">Phone number</label>
-          <div className="col-sm-3">
-            <div className="input-group">
-              <span className="input-group-addon">{prefix}</span>
-              <input type="text" className="form-control" id="input-text" />
-            </div>
+      </div>
+      <div className="form-group">
+        <label htmlFor="input-text" className="col-sm-3 control-label">Phone number</label>
+        <div className="col-sm-3">
+          <div className="input-group">
+            <span className="input-group-addon">{prefix}</span>
+            <input type="text" className="form-control" id="input-text" />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Prefixer;
